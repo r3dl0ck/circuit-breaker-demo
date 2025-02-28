5c58242651b3c6706f8f3451ba2826852328f269
Marcin Witowski <mars3n@protonmail.com>
2025-02-28 22:31:47 +0100

    adding yaml

--- src/circuit_breaker_lab/__init__.py
+++ src/circuit_breaker_lab/__init__.py
@@ -42,37 +42,66 @@ def health():
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description='Circuit Breaker App')
-    parser.add_argument('--flask-debug', dest='flask_debug', action='store_true',
-                        help='Enable Flask debug mode')
-    parser.add_argument('--no-flask-debug', dest='flask_debug', action='store_false',
-                        help='Disable Flask debug mode')
-    parser.set_defaults(flask_debug=os.getenv('FLASK_DEBUG', 'False').lower() in ['true', '1', 't'])
+    parser = argparse.ArgumentParser(description="Circuit Breaker App")
+    parser.add_argument(
+        "--flask-debug",
+        dest="flask_debug",
+        action="store_true",
+        help="Enable Flask debug mode",
+    )
+    parser.add_argument(
+        "--no-flask-debug",
+        dest="flask_debug",
+        action="store_false",
+        help="Disable Flask debug mode",
+    )
+    parser.set_defaults(
+        flask_debug=os.getenv("FLASK_DEBUG", "False").lower() in ["true", "1", "t"]
+    )
 
-    parser.add_argument('--flask-port', dest='flask_port', type=int,
-                        default=int(os.getenv('FLASK_PORT', 8080)),
-                        help='Port to run Flask app on')
+    parser.add_argument(
+        "--flask-port",
+        dest="flask_port",
+        type=int,
+        default=int(os.getenv("FLASK_PORT", 8080)),
+        help="Port to run Flask app on",
+    )
 
-    parser.add_argument('--host', dest='host', type=str,
-                        default='0.0.0.0',
-                        help='Host IP to listen on')
+    parser.add_argument(
+        "--host", dest="host", type=str, default="0.0.0.0", help="Host IP to listen on"
+    )
 
-    parser.add_argument('--log-level', dest='log_level', type=str,
-                        default=os.getenv('LOG_LEVEL', 'INFO').upper(),
-                        help='Logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)')
+    parser.add_argument(
+        "--log-level",
+        dest="log_level",
+        type=str,
+        default=os.getenv("LOG_LEVEL", "INFO").upper(),
+        help="Logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)",
+    )
 
-    parser.add_argument('--rate-limit-period', dest='rate_limit_period', type=int,
-                        default=int(os.getenv('RATE_LIMIT_PERIOD', 5)),
-                        help='Rate limit period in seconds')
+    parser.add_argument(
+        "--rate-limit-period",
+        dest="rate_limit_period",
+        type=int,
+        default=int(os.getenv("RATE_LIMIT_PERIOD", 5)),
+        help="Rate limit period in seconds",
+    )
 
-    parser.add_argument('--max-requests', dest='max_requests', type=int,
-                        default=int(os.getenv('MAX_REQUEST', 1)),
-                        help='Max requests allowed in rate limit period')
+    parser.add_argument(
+        "--max-requests",
+        dest="max_requests",
+        type=int,
+        default=int(os.getenv("MAX_REQUEST", 1)),
+        help="Max requests allowed in rate limit period",
+    )
 
-    parser.add_argument('--message', dest='message', type=str,
-                        default="Hello World!",
-                        help='Reply message')
-
+    parser.add_argument(
+        "--message",
+        dest="message",
+        type=str,
+        default="Hello World!",
+        help="Reply message",
+    )
 
     args = parser.parse_args()
 
@@ -87,4 +116,6 @@ def main() -> None:
     RATE_LIMIT_PERIOD = args.rate_limit_period
     MESSAGE = args.message
 
-    app.run(host=args.host, port=args.flask_port, debug=args.flask_debug, use_reloader=False)
+    app.run(
+        host=args.host, port=args.flask_port, debug=args.flask_debug, use_reloader=False
+    )
